e040d8fc229224dde99a70beb736826b6bb720e5
macwlu97 <maciej.wnuklipinski@gmail.com>
2026-03-09 14:11:39 +0100

    init

---
+++ src/main/java/com/ballotbox/ballot_box_api/BallotBoxApiApplication.java
@@ -0,0 +1,13 @@
+package com.ballotbox.ballot_box_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallotBoxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BallotBoxApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ballotbox/ballot_box_api/BallotBoxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ballotbox.ballot_box_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallotBoxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
